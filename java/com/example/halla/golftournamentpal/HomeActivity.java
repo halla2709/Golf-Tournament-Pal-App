b413a4c6a20932e912922f4105a1e09f4772cc9c
Hafrún <hah78@hi.is>
2017-02-14 17:48:18 +0000

    MatchPlayCreator

--- java/com/example/halla/golftournamentpal/HomeActivity.java
+++ java/com/example/halla/golftournamentpal/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.halla.golftournamentpal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void createMatchPlayTournament(View view){
+        Intent intent = new Intent(this, MatchPlayCreator.class);
+        startActivity(intent);
     }
 }
