112924472e3392f50597c682d2f3b7c34bf2380c
Hafrún <hah78@hi.is>
2017-03-15 21:52:56 +0000

    no message

--- java/com/example/halla/golftournamentpal/views/MyTournamentsActivity.java
+++ java/com/example/halla/golftournamentpal/views/MyTournamentsActivity.java
@@ -1,6 +1,7 @@
 package com.example.halla.golftournamentpal.views;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,18 +9,29 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.halla.golftournamentpal.Networker;
 import com.example.halla.golftournamentpal.R;
 import com.example.halla.golftournamentpal.SessionManager;
+import com.example.halla.golftournamentpal.TournamentArrayAdapter;
+import com.example.halla.golftournamentpal.models.Tournament;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyTournamentsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button mCreateButton;
     private SessionManager mSessionManager;
+    private List<Tournament> mTournamentList = new ArrayList<>();
+    private ListView mListView;
+    private TournamentArrayAdapter mAdapter;
 
     public void goBack (View view){
         // Button Listener
@@ -44,6 +56,12 @@ public class MyTournamentsActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mListView = (ListView) findViewById(R.id.my_tournaments_list);
+        mAdapter = new TournamentArrayAdapter(getApplicationContext());
+
+        MyTournamentsActivity.FetchMyTournamentsTask task = new MyTournamentsActivity.FetchMyTournamentsTask();
+        task.execute();
 
         mSessionManager = new SessionManager(getApplicationContext());
         if(mSessionManager.getSessionUserSocial() == 0) {
@@ -93,5 +111,30 @@ public class MyTournamentsActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class FetchMyTournamentsTask extends AsyncTask<Void, Void, List<Tournament>> {
+
+        @Override
+        protected List<Tournament> doInBackground(Void... params) {
+            Log.i("TAGG", "Fetching...");
+            return new Networker().fetchTournaments();
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.i("TAGG", "Going to fetch...");
+        }
+
+        @Override
+        protected void onPostExecute(List<Tournament> tournaments) {
+            mTournamentList = tournaments;
+            mAdapter.setData(mTournamentList);
+            mListView.setAdapter(mAdapter);
+            Log.i("TAGG", tournaments.get(0).getStartDate().toString());
+
+        }
     }
 }
