7f55937c05f470712ec20806e691305dbf8db8f2
Hafrún <hah78@hi.is>
2017-03-23 19:16:28 +0000

    Sýnir vini, ekki komið search eða profile síða

--- java/com/example/halla/golftournamentpal/views/MyFriendsActivity.java
+++ java/com/example/halla/golftournamentpal/views/MyFriendsActivity.java
@@ -2,6 +2,7 @@ package com.example.halla.golftournamentpal.views;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,18 +10,35 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.halla.golftournamentpal.Networker;
 import com.example.halla.golftournamentpal.R;
 import com.example.halla.golftournamentpal.SessionManager;
+import com.example.halla.golftournamentpal.models.Golfer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MyFriendsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button mCreateButton;
     private SessionManager mSessionManager;
+    private Golfer mGolfer;
+    private Long mUserSocial;
+    GolferArrayAdapter mParticipantAdapter;
+    ListView mParticipantListView;
+    List<Golfer> mParticipants = new ArrayList<>();
 
     public static Intent newIntent(Context packageContext) {
         Intent i = new Intent(packageContext, MyFriendsActivity.class);
@@ -36,12 +54,12 @@ public class MyFriendsActivity extends AppCompatActivity
         //startActivity(i);
     }
 
-    public void viewfriend (View view){
+    /*public void viewfriend (View view){
         mCreateButton = (Button) findViewById(R.id.viewbutton);
 
         Intent i = FriendProfileActivity.newIntent(MyFriendsActivity.this);
         startActivity(i);
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +82,34 @@ public class MyFriendsActivity extends AppCompatActivity
             Intent i = LogInActivity.newIntent(MyFriendsActivity.this);
             startActivity(i);
         }
+
+        mUserSocial = mSessionManager.getSessionUserSocial();
+
+        GetFriendsTask task = new GetFriendsTask();
+        task.execute();
+        try {
+            task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        displayFriends();
     }
+
+    public void displayFriends() {
+
+        mParticipantAdapter = new GolferArrayAdapter(this.getApplicationContext(), MyFriendsActivity.this);
+        mParticipantListView = (ListView) findViewById(R.id.myFriendsList);
+
+        mParticipants = mGolfer.getFriends();
+
+        mParticipantAdapter.setData(mParticipants);
+        mParticipantListView.setAdapter(mParticipantAdapter);
+        setListHeight(mParticipantListView);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -106,5 +151,84 @@ public class MyFriendsActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+    public void setListHeight(ListView list) {
+        MyFriendsActivity.GolferArrayAdapter adapter = (MyFriendsActivity.GolferArrayAdapter) list.getAdapter();
+        int totalHeight = 0;
+        for (int i = 0; i < mParticipants.size(); i++) {
+            View listItem = adapter.getView(i, null, list);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = list.getLayoutParams();
+        params.height = totalHeight
+                + (list.getDividerHeight() * (list.getCount() - 1));
+        list.setLayoutParams(params);
+    }
+
+    private class GolferArrayAdapter extends ArrayAdapter<Golfer> {
+
+
+        private final LayoutInflater mInflater;
+        private MyFriendsActivity mFragment;
+
+        public GolferArrayAdapter(Context context, MyFriendsActivity myFragment) {
+            super(context, R.layout.friend_list_item);
+            mFragment = myFragment;
+            mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void setData(List<Golfer> data) {
+            clear();
+            if (data != null) {
+                for (Golfer appEntry : data) {
+                    add(appEntry);
+                }
+            }
+        }
+
+        @Override
+        public View getView(int position, View convertView, final ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                view = mInflater.inflate(R.layout.friend_list_item, parent, false);
+            } else {
+                view = convertView;
+            }
+
+            final Golfer friend = getItem(position);
+            ((TextView) view.findViewById(R.id.friendName)).setText(friend.getName());
+            ((TextView) view.findViewById(R.id.friendSocial)).setText(Long.toString(friend.getSocial()));
+            ((TextView) view.findViewById(R.id.friendHandicap)).setText(Double.toString(friend.getHandicap()));
+            return view;
+        }
+    }
+
+    private class GetFriendsTask extends AsyncTask<Void, Void, Golfer> {
+
+        @Override
+        protected Golfer doInBackground(Void... params) {
+            Log.i("TAGG", "Fetching...");
+            mGolfer = new Networker().fetchGolfer(mUserSocial);
+            Log.i("TAGG", "Done fetching");
+            return mGolfer;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.i("TAGG", "Going to fetch...");
+        }
+
+        @Override
+        protected void onPostExecute(Golfer golfer) {
+            super.onPostExecute(golfer);
+            Log.i("TAGG", "Done");
+        }
     }
 }
