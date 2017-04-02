4f266d46864f7d17a7c9bcd1d0d9ebda78690def
Hafrún <hah78@hi.is>
2017-04-02 22:52:32 +0000

    Sorted Scoreboard JEIJ

--- java/com/example/halla/golftournamentpal/views/ScoreboardActivity.java
+++ java/com/example/halla/golftournamentpal/views/ScoreboardActivity.java
@@ -27,7 +27,15 @@ import com.example.halla.golftournamentpal.models.Scorecard;
 import com.example.halla.golftournamentpal.models.Tournament;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ScoreboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -123,10 +131,63 @@ public class ScoreboardActivity extends AppCompatActivity
         return true;
     }
 
+    public HashMap<Long, Integer> sortHashMap(ScoreboardTournament tournament){
+        HashMap<Long, Integer> hashMap = new HashMap<Long, Integer>() ;
+        for (int i=0; i<tournament.getPlayers().size(); i++){
+            long social = tournament.getScorecards().get(i).getPlayer().getSocial();
+            int total = tournament.getScorecards().get(i).getTotalForScorecard();
+            hashMap.put(social,total);
+        }
+
+        HashMap<Long, Integer> sortedMap = new HashMap<Long, Integer>() ;
+        sortedMap = sortByValues(hashMap);
+        Log.i("Hash Map", sortByValues(hashMap).toString());
+        return sortedMap;
+    }
+
+    private static HashMap sortByValues(HashMap map) {
+        List list = new LinkedList(map.entrySet());
+        // Defined Custom Comparator here
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                        .compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+
+        // Here I am copying the sorted list in HashMap
+        // using LinkedHashMap to preserve the insertion order
+        HashMap sortedHashMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedHashMap;
+    }
+
     public TableLayout fillTable(final ScoreboardTournament tournament, TableLayout tableLayout)
     {
+        HashMap<Long, Integer> hashMap = sortHashMap(tournament);
+        List<Scorecard> mSortedCards = new ArrayList<Scorecard>();
         mScorecards = tournament.getScorecards();
-        Log.i("Scorecards ", mScorecards.toString());
+
+        Set set = hashMap.entrySet();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry me = (Map.Entry) iterator.next();
+            long social = Long.parseLong(me.getKey().toString());
+            for (int j = 0; j<mScorecards.size(); j++) {
+                long socialCheck = mScorecards.get(j).getPlayer().getSocial();
+                if (socialCheck == social) {
+                    mSortedCards.add(mScorecards.get(j));
+                    break;
+                }
+            }
+        }
+
+        for (int x = 0; x < mSortedCards.size(); x++) {
+            Log.i("Sorted:", mSortedCards.get(x).getPlayer().getName().toString() + " " + mSortedCards.get(x).getTotalForScorecard()) ;
+        }
 
         tableLayout.setStretchAllColumns(true);
 
@@ -182,12 +243,12 @@ public class ScoreboardActivity extends AppCompatActivity
 
             TextView playerTextViewTable = new TextView(this);
             playerTextViewTable.setTextSize(TypedValue.COMPLEX_UNIT_SP, FONT_SIZE);
-            playerTextViewTable.setText(mScorecards.get(x).getPlayer().getName());
+            playerTextViewTable.setText(mSortedCards.get(x).getPlayer().getName());
             tableRow.addView(playerTextViewTable);
             playerTextViewTable.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
             playerTextViewTable.setTextColor(getResources().getColor(R.color.buttonTextColor));
 
-            final Scorecard scorecard = mScorecards.get(x);
+            final Scorecard scorecard = mSortedCards.get(x);
             for(int i = 0; i < tournament.getNumberOfRounds(); i++) {
                 final int round = i;
                 TextView resultTextView = new TextView(this);
